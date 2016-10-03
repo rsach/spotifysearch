308db0c83b09821b717c271ac41bcb9b84faf997
rahul sachdeva <rahul sachdeva>
2016-10-03 17:22:20 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { ArtistComponent } from './artist/artist.component';
 import { AlbumComponent } from './album/album.component';
 
 const routes: Routes = [
- { path: '', component: SearchComponent },
+{ path: '', redirectTo: 'search', pathMatch:'full' },
+ { path: 'search', component: SearchComponent },
   { path: 'about', component: AboutComponent },
   { path: 'artist/:id', component: ArtistComponent },
    { path: 'album/:id', component: AlbumComponent }
